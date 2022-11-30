7361b7960cf9eed3f93b032ffba24e34dfadd6df
Nata Nael <nata.nael@shopee.com>
2022-11-30 13:30:56 +0700

    add: storing notes and tags to localStorage

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,10 +1,34 @@
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "./App";
+import { v4 as uuidV4 } from "uuid";
 
-export function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    onSubmit({
+      // [!] means that the value could be null
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: selectedTags,
+    })
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
@@ -12,7 +36,7 @@ export function NoteForm() {
               <Form.Label>
                 Title
               </Form.Label>
-              <Form.Control required />
+              <Form.Control ref={titleRef} required />
             </Form.Group>
           </Col>
           <Col>
@@ -20,7 +44,27 @@ export function NoteForm() {
               <Form.Label>
                 Tags
               </Form.Label>
-              <CreatableReactSelect isMulti />
+              <CreatableReactSelect 
+                value={selectedTags.map(tag => {
+                  return { label: tag.label, value: tag.id }
+                })}
+                onChange={tags => {
+                  setSelectedTags(tags.map(tag => {
+                    return { label: tag.label, id: tag.value }
+                  }))
+                }}
+                onCreateOption={
+                  label => {
+                    const newTag = { id: uuidV4(), label }
+                    onAddTag(newTag)
+                    setSelectedTags(prev => [...prev, newTag])
+                  }
+                }
+                options={availableTags.map(tag => {
+
+                })}
+                isMulti 
+              />
             </Form.Group>
           </Col>
         </Row>
@@ -28,7 +72,7 @@ export function NoteForm() {
           <Form.Label>
             Markdown
           </Form.Label>
-          <Form.Control required as="textarea" rows={15} />
+          <Form.Control ref={markdownRef} required as="textarea" rows={15} />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">Save</Button>
